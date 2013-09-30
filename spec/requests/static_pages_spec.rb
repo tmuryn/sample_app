da67c7166988fe8018f309537d70de380ab52b98
Taras Muryn <taras.muryn@sethq.com>
2013-09-30 11:53:55 +0300

    add contacts page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('My Sample of text')
 		end
+		it "Should contain correct title: 'Ruby Rails App | Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title('Ruby Rails App | Home')
+		end
 	end
 
 	describe "Help page" do
@@ -16,26 +20,18 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('This is the help content of my help page')
 		end
+		it "Should contain correct title: 'Ruby Rails App | Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('Ruby Rails App | Help')
+		end
 	end
 
 	describe "About page" do
 
 		it "Should contain text 'About page is intended to answer most common questions'" do
-
 			visit '/static_pages/about'
 			expect(page).to have_content('About page is intended to answer most common questions')
 		end
-	end
-
-	describe "Home title test" do
-
-		it "Should contain correct title: 'Ruby Rails App | Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title('Ruby Rails App | Home')
-		end
-	end
-
-	describe "About title test" do
 
 		it "Should contain correct title: 'Ruby Rails App | About'" do
 			visit '/static_pages/about'
@@ -43,12 +39,14 @@ describe "Static pages" do
 		end
 	end
 
-	describe "Help title test" do
-
-		it "Should contain correct title: 'Ruby Rails App | Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Ruby Rails App | Help')
+	describe "Contact page" do
+		it "Should contain text 'On this page you can find contacts'" do
+			visit '/static_pages/contacts'
+			expect(page).to have_content('On this page you can find contacts')
+		end
+		it "Should contain title: 'Ruby Rails App | Contacts'" do
+			visit '/static_pages/contacts'
+			expect(page).to have_title('Ruby Rails App | Contacts')
 		end
 	end
-
 end
